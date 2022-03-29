c021e330e5a289c7a44dd81bc401cd2248e12551
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-29 15:07:02 -0300

    first commit

---
+++ src/main/java/com/mercadolivre/grupo4/desafiotesting/DesafioTestingApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.grupo4.desafiotesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadolivre/grupo4/desafiotesting/DesafioTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.grupo4.desafiotesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
